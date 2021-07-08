e9bf8db23b212f974eff06cbde6dd315137f8db4
dantasdd <45635642+dantasdd@users.noreply.github.com>
2021-07-08 17:14:46 -0300

    consulta.py
    Ajustes de codigo

--- consulta.py
+++ consulta.py
@@ -1,9 +1,11 @@
+#DECLARANDO BIBLIOTECAS NECESSARIAS PARA BUILD DE SCRIPT
 import pandas as pd
 from google.cloud import bigquery
 from google.oauth2 import service_account
 
+#EXECULTANDO CONSULTA AO BIGQUERY.
 query = """    
-SELECT channelGrouping AS Agrupamento_de_Canais,
+SELECT channelGrouping AS Agrupamento_de_Canais, 
  CONCAT(trafficsource.source, trafficsource.medium)
   AS Origem_midia,
   trafficsource.campaign AS Campanha,
@@ -17,30 +19,37 @@ FROM `bigquery-public-data.google_analytics_sample.ga_sessions_2017*`
   ORDER BY DATE DESC 
 """
 
+#FUNCAO QUE GERA UM ARQUIVO .CSV 
 def geraCSV(objeto, caminho):
     objeto.to_csv(caminho,index=False, encoding='utf-8', sep = ',') 
-
 
 def executaConsulta(sql):
     # AQUI ESTOU CRIANDO UM OBJETO COM AS CREDENCIAIS
     credentials = service_account.Credentials.from_service_account_file('blinks-essence-319111-965ef6aefac6.json')
     project_id = 'blinks-essence-319111'
+    
     # AQUI ESTOU INSTANCIANDO O OBJETO COM AS CREDENCIAIS E O PROJETO
     client = bigquery.Client(credentials= credentials,project=project_id)
-    print(client)   
-    # EXECUTANDO QUERY NO CLOUD
+    print(client) 
+    
+    # EXECUTANDO UMA QUERY NO BIGQUERY
     query = client.query(sql)
-    #PEGANDOS OS RESULTADOS 
+    
+    #RETORNANDO OS RESULTADOS 
     results =  query.result()    
     df = results.to_dataframe()
     return df
+   
+   #INSTANCIANDO O ARQUIVO .CSV ATRIBUINDO AO MESMO RESULTADOS DA QUERY SEPARANDO OS DADOS POR VIRGULA
     # df.to_csv('teste.csv',index=False, encoding='utf-8', sep = ',') 
     # for row in results:
     #     print(row)
     # return results
-
+    
+#RETORNANDO UM OBJETO COM A CONSULTA
 retorno = executaConsulta(query)
-geraCSV(retorno, "blinks.csv")
 
+# METODO RETORNA O ARQUIVO .CSV
+geraCSV(retorno, "blinks.csv")
 print(retorno)
 
